3cb1880074cfd19b87a0acf1c7653afb0afebaa4
srilekha-123 <85286664+srilekha-123@users.noreply.github.com>
2023-05-05 14:22:50 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,26 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  template: `<h1>Angular JS Bindings!</h1><br>
-            <span>{{str1}} :{{name}}</span><br>
-            <span>To Day Current Date:{{today}}</span>
-            <img src="{{imageSrc}}" />
-            <br>
-            <p>{{str2 + getLikes(likes)}}</p>
-  `,
-  styles:[`
-  h1,span {
-                font-weight: bold;
-                border: 1px ridge blue;
-                padding: 5px;
-                background-color:green;
-  }
-  img{
-     width: 300px;
-     height: auto;
-     }
-     p{
-     font-size: 35px;
-     color: darkBlue;
-    }
-
-
-  `]
-
-})
-export class AppComponent {
-  title = 'rvr';
-  today : Date;
-  constructor(){
-    this.today=new Date();
-  }
-  str1: string = "Hello my name is"
-  name: string = "Srilekha"
-  str2: string = "I like to"
-  likes: string[] = ['Cycle', "Bike", "Jeep"]
-  
-  getLikes = function(arr: any){
-  var arrString = arr.join(", ");
-  return " " + arrString
-   }
-   imageSrc: string = "../assets/snow.jpg";
-}
+ @Component({
+ selector: 'app-root',
+ template: `
+ <img [src]="myPic"/>
+ <br>
+ <button [disabled]="isEnabled">Click me</button><hr>
+ <button disabled="{!isEnabled}">Click me</button><br>
+ <p [ngClass]="className">This is cool stuff</p>
+ `,
+ styles: [` img {
+            height: 1000px;
+            width: auto;
+            }
+ .myClass {
+ color: red;
+ font-size: 24px;
+ }
+ `]
+ })
+ export class AppComponent {
+ myPic: string = "../assets/snow.jpg";
+ isEnabled: boolean = false;
+ className: string = "myClass";
+ }
